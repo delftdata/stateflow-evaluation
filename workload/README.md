a5593765b8c305522c982fbcb4d1d39d2e2c7841
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-08-14 11:46:57 +0200

    update benchmark readme

--- workload/README.md
+++ workload/README.md
@@ -17,3 +17,45 @@ export PYFLINK_CLIENT_EXECUTABLE=~/Documents/stateflow-evaluation/venv/bin/pytho
 ```
 
 sudo ./wrk -t 4 -c 10 -d 30s -R 10 -s "./hotel.lua" -L "http://localhost:8001"
+
+# Benchmark deployment details
+To deploy the benchmark we picked an EC2 instance, but this could be any compute instance. Its easiest if the instance
+has some default installations, so we picked an Ubuntu distribution. Then we installed `wrk2`:
+```bash
+https://github.com/giltene/wrk2.git
+sudo apt update
+sudo apt-get install build-essential
+sudo apt-get install libssl-dev
+sudo apt-get install zlib1g-dev
+cd wrk2
+make
+```
+This will build a `wrk` executable. Finally we install a `lua` dependency:
+```
+sudo apt-get update -y
+sudo apt-get install -y luarocks
+sudo luarocks install luasocket
+```
+
+## Experiments
+First, copy all `.lua` files to the `wrk2` folder on your instance. Then, we run two kind of experiments:
+1. A low throughput experiment, where we benchmark the runtime per endpoint. The commands for that are:
+```bash
+./wrk -t 4 -c 10 -d 30s -R 10 -s './hotel_user.lua' -L '{LOADBALANCER_URL}'
+./wrk -t 4 -c 10 -d 30s -R 10 -s './hotel_search.lua' -L '{LOADBALANCER_URL}'
+./wrk -t 4 -c 10 -d 30s -R 10 -s './hotel_recommend.lua' -L '{LOADBALANCER_URL}'
+./wrk -t 4 -c 10 -d 30s -R 10 -s './hotel_reserve.lua' -L '{LOADBALANCER_URL}'
+```
+This will send 10rps for 30 seconds, per endpoint, and report the latency for different percentiles.
+2. We keep increasing the throughput `[100, 200, 300, 400, 500, 600, 700]` and measure latency for 60 seconds.
+```bash
+./wrk -t 8 -c 200 -d 60s -R 100 -s './hotel.lua' -L '{LOADBALANCER_URL}'
+./wrk -t 8 -c 300 -d 60s -R 200 -s './hotel.lua' -L '{LOADBALANCER_URL}'
+./wrk -t 8 -c 400 -d 60s -R 300 -s './hotel.lua' -L '{LOADBALANCER_URL}'
+./wrk -t 8 -c 500 -d 60s -R 400 -s './hotel.lua' -L '{LOADBALANCER_URL}'
+./wrk -t 8 -c 600 -d 60s -R 500 -s './hotel.lua' -L '{LOADBALANCER_URL}'
+./wrk -t 8 -c 700 -d 60s -R 600 -s './hotel.lua' -L '{LOADBALANCER_URL}'
+./wrk -t 8 -c 800 -d 60s -R 700 -s './hotel.lua' -L '{LOADBALANCER_URL}'
+```
+
+**Note** For AWS we ran everything _twice_ to deal with hot/cold starts. 
---
+++ workload/hotel_recommend.lua
@@ -0,0 +1,118 @@
+require "socket"
+math.randomseed(socket.gettime()*1000)
+math.random(); math.random(); math.random()
+
+local function get_user()
+  local id = math.random(0, 500)
+  local user_name = "TUDelft_" .. tostring(id)
+  local pass_word = ""
+  for i = 0, 9, 1 do
+    pass_word = pass_word .. tostring(id)
+  end
+  return user_name, pass_word
+end
+
+local function search_hotel()
+  local in_date = math.random(9, 23)
+  local out_date = math.random(in_date + 1, 24)
+
+  local in_date_str = tostring(in_date)
+  if in_date <= 9 then
+    in_date_str = "2015-04-0" .. in_date_str
+  else
+    in_date_str = "2015-04-" .. in_date_str
+  end
+
+  local out_date_str = tostring(out_date)
+  if out_date <= 9 then
+    out_date_str = "2015-04-0" .. out_date_str
+  else
+    out_date_str = "2015-04-" .. out_date_str
+  end
+
+  local lat = 38.0235 + (math.random(0, 481) - 240.5)/1000.0
+  local lon = -122.095 + (math.random(0, 325) - 157.0)/1000.0
+
+  local method = "GET"
+  local path = "/search?in_date=" .. in_date_str ..
+    "&out_date=" .. out_date_str .. "&lat=" .. tostring(lat) .. "&lon=" .. tostring(lon)
+
+  local headers = {}
+  -- headers["Content-Type"] = "application/x-www-form-urlencoded"
+  return wrk.format(method, path, headers, nil)
+end
+
+local function recommend()
+  local coin = math.random()
+  local req_param = ""
+  if coin < 0.33 then
+    req_param = "DISTANCE"
+  elseif coin < 0.66 then
+    req_param = "RATE"
+  else
+    req_param = "PRICE"
+  end
+
+  local lat = 38.0235 + (math.random(0, 481) - 240.5)/1000.0
+  local lon = -122.095 + (math.random(0, 325) - 157.0)/1000.0
+
+  local method = "GET"
+  local path = "/recommend?recommendation_type=" .. req_param ..
+    "&lat=" .. tostring(lat) .. "&lon=" .. tostring(lon)
+  local headers = {}
+  -- headers["Content-Type"] = "application/x-www-form-urlencoded"
+  return wrk.format(method, path, headers, nil)
+end
+
+local function reserve()
+  local in_date = math.random(9, 23)
+  local out_date = in_date + math.random(1, 5)
+
+  local in_date_str = tostring(in_date)
+  if in_date <= 9 then
+    in_date_str = "2015-04-0" .. in_date_str
+  else
+    in_date_str = "2015-04-" .. in_date_str
+  end
+
+  local out_date_str = tostring(out_date)
+  if out_date <= 9 then
+    out_date_str = "2015-04-0" .. out_date_str
+  else
+    out_date_str = "2015-04-" .. out_date_str
+  end
+
+  local hotel_id = tostring(math.random(1, 80))
+  local user_id, password = get_user()
+  local cust_name = user_id
+
+  local num_room = "1"
+
+  local method = "GET"
+  local path = "/reserve?in_date=" .. in_date_str ..
+    "&out_date=" .. out_date_str ..
+    "&hotel_id=" .. hotel_id .. "&customer_name=" .. cust_name .. "&username=" .. user_id ..
+    "&password=" .. password .. "&amount_of_rooms=" .. num_room
+  local headers = {}
+  -- headers["Content-Type"] = "application/x-www-form-urlencoded"
+  return wrk.format(method, path, headers, nil)
+end
+
+local function user_login()
+  local user_name, password = get_user()
+  local method = "GET"
+  local path = "/login?username=" .. user_name .. "&password=" .. password
+  local headers = {}
+  -- headers["Content-Type"] = "application/x-www-form-urlencoded"
+  return wrk.format(method, path, headers, nil)
+end
+
+request = function()
+  cur_time = math.floor(socket.gettime())
+  local search_ratio      = 0.6
+  local recommend_ratio   = 0.39
+  local user_ratio        = 0.005
+  local reserve_ratio     = 0.005
+
+  return recommend()
+end
